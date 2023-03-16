c63d6346dab0008c14f7d24497fe249b57f81d9f
tony555t <tonytullah764@gmail.com>
2023-03-16 13:39:33 +0300

    strong params

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,10 +8,9 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(params.permit(:name, :species))
     render json: bird, status: :created
   end
-
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
